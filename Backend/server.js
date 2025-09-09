a3dea312953a47a7526b347ddaf6a3a9ce47e587
Naresh5757 <nareshsai57575@gmail.com>
2025-09-09 11:18:26 +0530

    project completed

---
+++ Backend/server.js
@@ -0,0 +1,46 @@
+import express from "express"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+import authRoutes from "./routes/auth.route.js"
+import ProductsRoutes from "./routes/products.route.js"
+import cartRoutes from "./routes/cart.route.js"
+import couponRoutes from "./routes/coupon.route.js"
+import paymentRoutes from "./routes/paymnet.route.js"
+import analyticRoutes from "./routes/analytic.route.js"
+import path from "path"
+import cors from "cors"
+import { connectDB } from "./lib/db.js"
+dotenv.config()
+const app=express()
+console.log(process.env.CLOUDINARY_CLOUD_NAME, process.env.STRIPE_SECRET_KEY);
+// console.log(process.env.PORT)
+const PORT=process.env.PORT ||5000;
+
+// const __dirname = path.resolve()
+
+app.use(cors({
+    origin:["http://localhost:5173"], credentials:true
+}))
+
+app.use(express.json({limit:"10mb"}))
+app.use(cookieParser())
+
+
+app.use("/api/auth",authRoutes )
+app.use("/api/products",ProductsRoutes)
+app.use("/api/cart",cartRoutes)
+app.use("/api/coupons",couponRoutes)
+app.use("/api/payments",paymentRoutes)
+app.use("/api/analytics",analyticRoutes)
+
+// if (process.env.NODE_ENV === "production"){
+//     app.use(express.static(path.join(__dirname, "/Frontend/dist")));
+//     app.get("*", (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
+// 	});
+// }
+app.listen(PORT,()=>{
+    console.log("server is running on http://localhost:"+PORT);
+    connectDB()
+    
+})
